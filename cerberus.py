0d6716738c5c9993ef8a93783dbf5aaace460dfb
Roberto Sosa <Roberto.Sosa@citrix.com>
2021-10-11 22:25:30 -0400

    Test Case 1 Completed

--- cerberus.py
+++ cerberus.py
@@ -17,29 +17,47 @@ from client_credentials import *
 az_auth_args = 'az login --service-principal' \
     + ' -u ' + az_client_id + ' -p ' + az_client_secret + ' -t ' + az_tenant_id
 
-subprocess.run(az_auth_args, shell=True)
+# subprocess.run(az_auth_args, shell=True)
 
+print('\nCloud Cerberus checks starting...\n')
+
+print("""
+# -----------------------------------------------------------------------------
 # Test Case Example 1
 # Account Management
 # ID: NIST SP 800-53 Rev. 4 AC-2
 # Description: It is recommended to designate up to 3 subscription owners 
 # in order to reduce the potential for breach by a compromised owner.
 #
-# Possible Remediation Actions
+# Possible Remediating Actions:
 # az role assignment create --role "Owner" --assignee <USER> --subscription <ID>
+# -----------------------------------------------------------------------------
+""")
 
-subscriptions = json.loads(subprocess.check_output('az account list', shell=True))
+az_account_list = subprocess.check_output('az account list', shell=True)
+sub_list = json.loads(az_account_list)
 
-print(json.dumps(subscriptions, indent=4, sort_keys=True))
+print(json.dumps(sub_list, indent=4, sort_keys=True))
 
-print(type(subscriptions))
-print(len(subscriptions))
-print(subscriptions[0]['id'])
+# Since each account can have multiple subscriptions, append to a final list: 
+sub_ids = []
+for i in range(len(sub_list)):
+    sub_ids.append(sub_list[i]['id'])
 
-#sub_ids = []
-#for i in range(len(subscriptions)):
-#    sub_ids.append(subscriptions[i]['id'])
+for i in range(len(sub_ids)):
+    az_role_args = 'az role assignment list --subscription ' + sub_ids[i] \
+        + ' --role "Owner" --query \'[*].principalId\''
+    
+    az_role_check = subprocess.check_output(az_role_args, shell=True)
+    sub_owner_list = json.loads(az_role_check)
+    owner_count = len(sub_owner_list)
+    
+    print('\nSubscription ID:', sub_ids[i])
+    print(json.dumps(sub_owner_list, indent=4, sort_keys=True), '\n')
 
-# Test
-# print(sub_ids)
+    print('Total number of owners identified:', owner_count, '\n')
 
+    if owner_count <= 3:
+        print('Security Control Assessment: “Satisfied”\n')
+    else:
+        print('Security Control Assessment: “Other Than Satisfied”\n')
